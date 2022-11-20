1e9ffc4289d8d1d898e5438e30f6da8698d19f04
Андрей Ларин <andrewlarin17@gmail.com>
2022-11-20 23:01:53 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Linear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    struct Linear
+    {
+        double k;
+        double b;
+
+        public Linear(double k1, double b1)
+        {
+            this.k = k1;
+            this.b = b1;
+        }
+        public string Root()
+        {
+            if (k == 0 && b == 0)
+                return "Решений бесконечно много";
+            else if (k == 0)
+                return "Решений нет";
+            else return $"Решение уравнения: {-b/k}";
+        }
+
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linear linear = new Linear(k, b);
+            Console.WriteLine(linear.Root());
+        }
+    }
+}
